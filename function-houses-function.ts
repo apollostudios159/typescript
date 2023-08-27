d39f41791e641c3ba6d18a287e12d9bb80410a9e
apollostudios159 <apollostudios159@gmail.com>
2023-08-27 20:21:26 +0530

    houses function

---
+++ function-houses-function.ts
@@ -0,0 +1,31 @@
+import houses from "./houses";
+
+interface House {
+  name: string;
+  planets: string | string[];
+}
+
+interface HouseWithID {
+  id: number;
+  name: string;
+  planets: string | string[];
+}
+
+function findHouses(
+  input: string | House[],
+  filter: (house: House) => boolean
+): HouseWithID[] {
+  const houses: House[] = typeof input === "string" ? JSON.parse(input) : input;
+  return (filter ? houses.filter(filter) : houses).map((house) => {
+    return {
+      id: houses.indexOf(house),
+      ...house,
+    };
+  });
+}
+
+console.log(
+  findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
+);
+
+// console.log(findHouses(houses, ({ name }) => name === "Harkonnen"));
