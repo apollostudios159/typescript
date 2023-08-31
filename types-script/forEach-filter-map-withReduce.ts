0407f737a48fb81829430495672163bf18fe9471
apollostudios159 <apollostudios159@gmail.com>
2023-08-31 12:30:06 +0530

    map filter reduce for Each

---
+++ types-script/forEach-filter-map-withReduce.ts
@@ -0,0 +1,26 @@
+//Produce the following using filter. They have to be type safe
+//For Each
+//Filter
+//Map
+
+//ForEach
+function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
+  items.reduce((acc, val) => {
+    forEachFunc(val);
+    return undefined;
+  }, undefined);
+}
+myForEach(["a", "b", "c"], (v) => console.log(`forEach ${v}`));
+
+//Filter
+function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+  return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [] as T[]);
+}
+console.log(myFilter([1, 2, 3, 4, 5, 6], (v) => v % 2 === 0));
+
+//Map
+function myMap<T, K>(item: T[], mapFunc: (v: T) => K): K[] {
+  return item.reduce((acc, val) => [...acc, mapFunc(val)], [] as K[]);
+}
+
+console.log(myMap([1, 2, 3, 4, 5, 6], (v) => (v * 10).toString()));
